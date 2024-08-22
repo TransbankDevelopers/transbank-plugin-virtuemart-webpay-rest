e1806d54197d9272d4d3913813b486de54796b32
DiegoHBustamante <99495937+DiegoHBustamante@users.noreply.github.com>
2024-08-22 15:42:48 -0400

    feat: add getLastGitHubReleaseVersion function

--- src/transbank_webpay_rest/library/HealthCheck.php
+++ src/transbank_webpay_rest/library/HealthCheck.php
@@ -123,7 +123,18 @@ class HealthCheck
 
         return $result;
     }
-
+        private function getLastGitHubReleaseVersion($string): string{
+        $baseurl = 'https://api.github.com/repos/'.$string.'/releases/latest';
+        $agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)';
+        $ch = curl_init();
+        curl_setopt($ch, CURLOPT_URL, $baseurl);
+        curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, $agent);
+        $content = curl_exec($ch);
+        curl_close($ch);
+        $con = json_decode($content, true);
+        return $con['tag_name'] ?? '';
+    }
     // creacion de retornos
     // arma array que entrega informacion del ecommerce: nombre, version instalada, ultima version disponible
     private function getPluginInfo($ecommerce)
