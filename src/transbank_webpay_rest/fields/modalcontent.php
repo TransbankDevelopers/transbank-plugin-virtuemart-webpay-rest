c367c9b9502d0e19711f22c81a8633f093cbb67b
DiegoHBustamante <99495937+DiegoHBustamante@users.noreply.github.com>
2024-09-02 16:55:59 -0400

    feat: delete modal-footer

--- src/transbank_webpay_rest/fields/modalcontent.php
+++ src/transbank_webpay_rest/fields/modalcontent.php
@@ -502,11 +502,6 @@ if ($logs->config->status === true) {
                 </div>
                 <!--FIN modalbody-->
             </div>
-            <div class="modal-footer">
-                <a class="btn btn-danger btn-lg" id="boton_pdf" href="<?php echo $urlCreaPdfReport; ?>" target="_blank" rel="noopener">Crear PDF</a>
-                <a class="btn btn-danger btn-lg" id="boton_php_info" href="<?php echo $urlCreaPdfPhpInfo; ?>" target="_blank" rel="noopener">Crear PHP info</a>
-                <button type="button" class="btn btn-default btn-lg" data-dismiss="modal">Cerrar</button>
-            </div>
         </div>
     </div>
 </div>
